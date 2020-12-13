902bdd6823b32cff10c3ece6c0d0764ac32c7200
Gaming32 <soujournme@gmail.com>
2020-12-13 14:52:00 -0600

    added constants

--- pyasm/parse.py
+++ pyasm/parse.py
@@ -76,10 +76,12 @@ class Macro:
     ops = list[objasm.OpCode]
 
 
-def _parse_arg(arg: Union[ast.Name, ast.Constant], macargs=None) -> str:
+def _parse_arg(arg: Union[ast.Name, ast.Constant], macargs=None, consts=None) -> str:
     if isinstance(arg, ast.Name):
         if macargs is not None and arg.id in macargs:
             return [arg.id, '']
+        if consts is not None and arg.id in consts:
+            return consts[arg.id]
         return arg.id
     return str(arg.value)
 
@@ -100,17 +102,17 @@ def _fill_macro(macro: list[objasm.OpCode], args: dict[str, str]):
     return copy
 
 
-def _parse_pycall(call: ast.Call, macros: dict, macargs) -> list[objasm.OpCode]:
+def _parse_pycall(call: ast.Call, macros: dict, macargs, consts) -> list[objasm.OpCode]:
     fname = call.func.id
     if fname in macros:
-        return _fill_macro(macros[fname].ops, {ar: _parse_arg(ca, macargs) for (ar, ca) in zip(macros[fname].args, call.args)})
+        return _fill_macro(macros[fname].ops, {ar: _parse_arg(ca, macargs, consts) for (ar, ca) in zip(macros[fname].args, call.args)})
     elif fname in fns:
-        args = [_parse_arg(arg, macargs) for arg in call.args]
+        args = [_parse_arg(arg, macargs, consts) for arg in call.args]
         return [objasm.OpCode(op=fns[fname], args=args)]
     elif fname in dirimmfns:
         args = []
         for arg in call.args:
-            args.append(_parse_arg(arg, macargs))
+            args.append(_parse_arg(arg, macargs, consts))
             if type(args[-1]) == list:
                 args[-1][1] = '#'
             else:
@@ -131,7 +133,7 @@ def _parse_pycall(call: ast.Call, macros: dict, macargs) -> list[objasm.OpCode]:
                 call.lineno,
                 fname
             )
-        args = [_parse_arg(arg, macargs) for arg in call.args[1:]]
+        args = [_parse_arg(arg, macargs, consts) for arg in call.args[1:]]
         return [objasm.OpCode(op=regfns[fname][opix], args=args)]
     elif fname in immfns:
         regstr = call.args[0].id
@@ -149,7 +151,7 @@ def _parse_pycall(call: ast.Call, macros: dict, macargs) -> list[objasm.OpCode]:
             )
         args = []
         for arg in call.args[1:]:
-            args.append(_parse_arg(arg, macargs))
+            args.append(_parse_arg(arg, macargs, consts))
             if type(args[-1]) == list:
                 args[-1][1] = '#'
             else:
@@ -166,12 +168,12 @@ def _parse_pycall(call: ast.Call, macros: dict, macargs) -> list[objasm.OpCode]:
         )
 
 
-def _parse_code(code: Union[ast.Expr, ast.Pass, ast.Return], macros: dict, macargs) -> list[objasm.OpCode]:
+def _parse_code(code: Union[ast.Expr, ast.Pass, ast.Return], macros: dict, macargs, consts) -> list[objasm.OpCode]:
     if isinstance(code, ast.Pass):
         return [objasm.OpCode(op='nop', args=[])]
     elif isinstance(code, ast.Expr):
         if isinstance(code.value, ast.Call):
-            return _parse_pycall(code.value, macros, macargs)
+            return _parse_pycall(code.value, macros, macargs, consts)
         else:
             raise errors.UnsupportedSyntaxElement.create_custom(
                 'Expr.value',
@@ -190,13 +192,13 @@ def _parse_code(code: Union[ast.Expr, ast.Pass, ast.Return], macros: dict, macar
         )
 
 
-def _parse_function(fn: ast.FunctionDef, macros: dict, ismacro=False) -> objasm.Label:
+def _parse_function(fn: ast.FunctionDef, macros: dict, consts, ismacro=False) -> objasm.Label:
     result = objasm.Label(name=fn.name, codes=[])
     macargs = None
     if ismacro:
         macargs = [arg.arg for arg in fn.args.args]
     for body_part in fn.body:
-        result.codes.extend(_parse_code(body_part, macros, macargs))
+        result.codes.extend(_parse_code(body_part, macros, macargs, consts))
     if ismacro:
         newmac = Macro()
         newmac.args = macargs
@@ -222,13 +224,15 @@ def find_module(name: str) -> Union[str, None]:
 
 
 MacroDict = dict[str, Macro]
+ConstantDict = dict[str, str]
 LabelList = list[str]
 
 
-def parse2(root: ast.Module, _recurc: int = 0) -> tuple[objasm.Program, MacroDict, LabelList]:
+def parse2(root: ast.Module, _recurc: int = 0) -> tuple[objasm.Program, MacroDict, ConstantDict, LabelList]:
     result = objasm.Program(labels=[])
     reserved_labels = []
     macros = {}
+    consts = {}
     for branch in root.body:
         if type(branch) == ast.ImportFrom:
             if branch.module == 'pyasm.stubs':
@@ -239,9 +243,10 @@ def parse2(root: ast.Module, _recurc: int = 0) -> tuple[objasm.Program, MacroDic
                 result.labels.append(objasm.Label(name=f'___jts1_{_recurc}___', codes=[
                     objasm.OpCode(op='jmp', args=[f'___jts2_{_recurc}___'])
                 ]))
-                new_result, new_macros, new_reserved = parse2(new_module, _recurc + 1)
+                new_result, new_macros, new_consts, new_reserved = parse2(new_module, _recurc + 1)
                 result.labels.extend(new_result.labels)
                 macros.update(new_macros)
+                consts.update(new_consts)
                 reserved_labels.extend(new_reserved)
                 result.labels.append(objasm.Label(name=f'___jts2_{_recurc}___', codes=[]))
             else:
@@ -251,13 +256,13 @@ def parse2(root: ast.Module, _recurc: int = 0) -> tuple[objasm.Program, MacroDic
             for dec in branch.decorator_list:
                 if isinstance(dec, ast.Name) and dec.id == 'inline_macro':
                     macro = True
-            parse_result = _parse_function(branch, macros, macro)
+            parse_result = _parse_function(branch, macros, consts, macro)
             if not macro:
                 result.labels.append(parse_result)
         elif type(branch) == ast.Expr:
             if isinstance(branch.value, ast.Call):
                 if branch.value.func.id == 'reserve_label':
-                    reserved_labels.append(branch.value.args[0].value)
+                    reserved_labels.append(_parse_arg(branch.value.args[0], None, consts))
                 else:
                     raise errors.UnsupportedFunctionOrElement.create_custom(
                         'top-level function',
@@ -272,6 +277,9 @@ def parse2(root: ast.Module, _recurc: int = 0) -> tuple[objasm.Program, MacroDic
                     branch.value.col_offset,
                     branch.value
                 )
+        elif type(branch) == ast.Assign:
+            for targ in branch.targets:
+                consts[targ.id] = _parse_arg(branch.value, None, consts)
         else:
             raise errors.UnsupportedSyntaxElement.create_custom(
                 'body',
@@ -279,11 +287,11 @@ def parse2(root: ast.Module, _recurc: int = 0) -> tuple[objasm.Program, MacroDic
                 branch.col_offset,
                 branch
             )
-    return result, macros, reserved_labels
+    return result, macros, consts, reserved_labels
 
 
 def parse(root: ast.Module) -> objasm.Program:
-    result, macros, reserved_labels = parse2(root)
+    result, _, _, reserved_labels = parse2(root)
     result.labels.append(objasm.Label(name='___hlt___', codes=[]))
     for label in reserved_labels:
         result.labels.append(objasm.Label(name=label, codes=[]))
